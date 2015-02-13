1ce0fa568fe36702a562b5b9b609df6b694a2bf5
Mick van Gelderen <mickvangelderen@gmail.com>
2015-02-13 16:57:09 +0100

    Added round and pareto distance graphs

--- analyze_tournament.m
+++ analyze_tournament.m
@@ -5,23 +5,63 @@ agents = unique([data{11 + (1:nagentsPerSession)}]);
 nagents = length(agents);
 
 utilities = cell(1, nagents);
+rounds = cell(1, nagents);
+pareto = cell(1, nagents);
 
 % compute gained utility for each agent
-for iagents = 1:nagents
-    agent = agents{iagents};
+for iagent = 1:nagents
+    agent = agents{iagent};
+    
+    % Compute utilities
     accumulator = [];
     for iaps = 1:nagentsPerSession
         indices = strcmp(data{11 + iaps}, agent);
-        utils = data{11 + nagentsPerSession + iaps}(indices);
-        accumulator = [accumulator;utils];
+        values = data{11 + nagentsPerSession + iaps}(indices);
+        accumulator = [accumulator;values];
     end
-    utilities{iagents} = accumulator;
+    utilities{iagent} = accumulator;
+    
+    % Compute rounds
+    accumulator = [];
+    for iaps = 1:nagentsPerSession
+        indices = strcmp(data{11 + iaps}, agent);
+        values = data{3}(indices);
+        accumulator = [accumulator;values];
+    end
+    rounds{iagent} = accumulator;
+    
+    % Compute distance to pareto
+    accumulator = [];
+    for iaps = 1:nagentsPerSession
+        indices = strcmp(data{11 + iaps}, agent);
+        values = data{9}(indices);
+        accumulator = [accumulator;values];
+    end
+    pareto{iagent} = accumulator;
 end
 
 agents'
+
 means = mean(cell2mat(utilities));
 stddevs = std(cell2mat(utilities));
 figure
 hold on
 bar(1:nagents, means)
 errorbar(1:nagents, means, stddevs, '.')
+hold off
+
+means = mean(cell2mat(rounds));
+stddevs = std(double(cell2mat(rounds)));
+figure
+hold on
+bar(1:nagents, means)
+errorbar(1:nagents, means, stddevs, '.')
+hold off
+
+means = mean(cell2mat(pareto));
+stddevs = std(cell2mat(pareto));
+figure
+hold on
+bar(1:nagents, means)
+errorbar(1:nagents, means, stddevs, '.')
+hold off
