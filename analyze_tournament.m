d5456dc4f2b860df315ddda88c338f8ed77e06ea
Mick van Gelderen <mickvangelderen@gmail.com>
2015-02-16 15:05:21 +0100

    Added stuff

--- analyze_tournament.m
+++ analyze_tournament.m
@@ -1,4 +1,5 @@
-data = read_tournament('Log-Tournament_20150213-154242.csv');
+filename = '1v1v1-180rounds-hard';
+data = read_tournament([filename '.csv']);
 
 nagentsPerSession = (length(data) - 11)/2;
 agents = unique([data{11 + (1:nagentsPerSession)}]);
@@ -7,6 +8,8 @@ nagents = length(agents);
 utilities = cell(1, nagents);
 rounds = cell(1, nagents);
 pareto = cell(1, nagents);
+nash = cell(1, nagents);
+welfare = cell(1, nagents);
 
 % compute gained utility for each agent
 for iagent = 1:nagents
@@ -38,30 +41,79 @@ for iagent = 1:nagents
         accumulator = [accumulator;values];
     end
     pareto{iagent} = accumulator;
+    
+    % Compute distance to nash
+    accumulator = [];
+    for iaps = 1:nagentsPerSession
+        indices = strcmp(data{11 + iaps}, agent);
+        values = data{10}(indices);
+        accumulator = [accumulator;values];
+    end
+    nash{iagent} = accumulator;
+    
+    % Compute welfare
+    accumulator = [];
+    for iaps = 1:nagentsPerSession
+        indices = strcmp(data{11 + iaps}, agent);
+        values = data{10}(indices);
+        accumulator = [accumulator;values];
+    end
+    welfare{iagent} = accumulator;
 end
 
 agents'
 
 means = mean(cell2mat(utilities));
 stddevs = std(cell2mat(utilities));
-figure
+figure('PaperPosition',  [.25 .25 8 6], 'PaperPositionMode', 'auto');
 hold on
 bar(1:nagents, means)
 errorbar(1:nagents, means, stddevs, '.')
+xlabel('Agent');
+ylabel('Utility');
+print([filename '-utility'], '-djpeg');
 hold off
 
 means = mean(cell2mat(rounds));
 stddevs = std(double(cell2mat(rounds)));
-figure
+figure('PaperPosition',  [.25 .25 8 6], 'PaperPositionMode', 'auto');
 hold on
 bar(1:nagents, means)
 errorbar(1:nagents, means, stddevs, '.')
+xlabel('Agent');
+ylabel('Number of Rounds');
+print([filename '-rounds'], '-djpeg');
 hold off
 
 means = mean(cell2mat(pareto));
 stddevs = std(cell2mat(pareto));
-figure
+figure('PaperPosition',  [.25 .25 8 6], 'PaperPositionMode', 'auto');
 hold on
 bar(1:nagents, means)
 errorbar(1:nagents, means, stddevs, '.')
+xlabel('Agent');
+ylabel('Distance to Pareto Frontier');
+print([filename '-pareto'], '-djpeg');
+hold off
+
+means = mean(cell2mat(nash));
+stddevs = std(cell2mat(nash));
+figure('PaperPosition',  [.25 .25 8 6], 'PaperPositionMode', 'auto');
+hold on
+bar(1:nagents, means)
+errorbar(1:nagents, means, stddevs, '.')
+xlabel('Agent');
+ylabel('Distance to Nash');
+print([filename '-nash'], '-djpeg');
+hold off
+
+means = mean(cell2mat(welfare));
+stddevs = std(cell2mat(welfare));
+figure('PaperPosition',  [.25 .25 8 6], 'PaperPositionMode', 'auto');
+hold on
+bar(1:nagents, means)
+errorbar(1:nagents, means, stddevs, '.')
+xlabel('Agent');
+ylabel('Social Welfare');
+print([filename '-welfare'], '-djpeg');
 hold off
